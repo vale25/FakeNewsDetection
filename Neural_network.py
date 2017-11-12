b1dbacdf84884b980e68ddb13f3ff7e98087cae4
vale25 <valentina.accadia@gmail.com>
2017-11-12 23:38:33 +0100

    aggiunto pretrained embedding

--- Neural_network.py
+++ Neural_network.py
@@ -11,16 +11,14 @@ import pickle
 
 t1 = datetime.now()
 with open("trainset_senza_duplicati.json") as f:
-    reviews = f.read().strip().split("\n")
+    articles = f.read().strip().split("\n")
 
-reviews = [json.loads(review) for review in reviews]
+articles = [json.loads(article) for article in articles]
 print(datetime.now() - t1)
 
-# Get a balanced sample of real and fake reviews
-texts = [review['title'] for review in reviews]
+texts = [article['text'] for article in articles]
 
-# Convert our 5 classes into 2 (negative or positive)
-binfake = [0 if review['label'] == "FAKE" else 1 for review in reviews]
+binfake = [0 if article['label'] == "FAKE" else 1 for article in articles]
 balanced_texts = []
 balanced_labels = []
 limit = 100000  # Change this to grow/shrink the dataset
@@ -35,13 +33,51 @@ print(balanced_labels)
 
 print(Counter(balanced_labels))
 
+# comincio ad addestrare
 tokenizer = Tokenizer(num_words=20000)
 tokenizer.fit_on_texts(balanced_texts)
 sequences = tokenizer.texts_to_sequences(balanced_texts)
 data = pad_sequences(sequences, maxlen=700)
 
+# uso pretrained embeddings con i dati di glove
+
+embeddings_index = {}
+glove_data = '/home/valentina/Documenti/tesi/testi/glove.6B.50d.txt'
+f = open(glove_data)
+for line in f:
+    values = line.split()
+    word = values[0]
+    value = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = value
+f.close()
+
+print('Loaded %s word vectors.' % len(embeddings_index))
+
+
+
+embedding_dimension = 50
+word_index = tokenizer.word_index
+
+embedding_matrix = np.zeros((len(word_index)+1, embedding_dimension))
+for word, i in word_index.items():
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        # words not found in embedding index will be all-zeros.
+        embedding_matrix[i] = embedding_vector[:embedding_dimension]
+
+print(embedding_matrix.shape)
+
+embedding_layer = Embedding(embedding_matrix.shape[0],
+                            embedding_matrix.shape[1],
+                            weights=[embedding_matrix],
+                            input_length=700)
+
+
 model = Sequential()
-model.add(Embedding(20000, 128, input_length=700))
+#model.add(Embedding(20000, 128, input_length=700))
+
+# utilizzo il layer embedded di glove
+model.add(embedding_layer)
 model.add(Dropout(0.2))
 model.add(Conv1D(64, 5, activation='relu'))
 model.add(MaxPooling1D(pool_size=4))
