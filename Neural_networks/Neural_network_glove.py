e4b9f8fee7219e8bf03a244e1094b7ebb3704df7
vale25 <valentina.accadia@gmail.com>
2017-11-20 16:42:46 +0100

    separate file for glove data

---
+++ Neural_networks/Neural_network_glove.py
@@ -0,0 +1,93 @@
+from collections import Counter
+from datetime import datetime
+import json
+from keras.layers import Embedding, LSTM, Dense, Conv1D, MaxPooling1D, Dropout, Activation
+from keras.models import Sequential
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+import numpy as np
+import pickle
+
+t1 = datetime.now()
+with open("/home/valentina/Documenti/tesi/trainset_senza_duplicati.json") as f:
+    articles = f.read().strip().split("\n")
+
+articles = [json.loads(article) for article in articles]
+print(datetime.now() - t1)
+
+texts = [article['text'] for article in articles]
+
+binfake = [0 if article['label'] == "FAKE" else 1 for article in articles]
+balanced_texts = []
+balanced_labels = []
+# usa questo parametro se il dataset e' molto grande per decidere quante news fake e real considerare
+limit = 10000
+neg_pos_counts = [0, 0]
+for i in range(len(texts)):
+    polarity = binfake[i]
+    if neg_pos_counts[polarity] < limit:
+        balanced_texts.append(texts[i])
+        balanced_labels.append(binfake[i])
+        neg_pos_counts[polarity] += 1
+print(balanced_labels)
+
+print(Counter(balanced_labels))
+
+# comincio ad addestrare
+
+# num_words: le prime n parole piu' frequenti vengono incluse nel vocabolario
+tokenizer = Tokenizer(num_words=30000)
+
+tokenizer.fit_on_texts(balanced_texts)
+sequences = tokenizer.texts_to_sequences(balanced_texts)
+print (len(tokenizer.word_index))
+data = pad_sequences(sequences, maxlen=1000)
+
+# uso pretrained embeddings con i dati di glove
+embeddings_index = {}
+glove_data = '/media/valentina/Data/pretrained_data/glove.6B.200d.txt'
+f = open(glove_data)
+for line in f:
+    values = line.split()
+    word = values[0]
+    value = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = value
+f.close()
+
+print('Loaded %s word vectors.' % len(embeddings_index))
+
+embedding_dimension = 50
+word_index = tokenizer.word_index
+
+embedding_matrix = np.zeros((len(word_index)+1, embedding_dimension))
+for word, i in word_index.items():
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        # words not found in embedding index will be all-zeros.
+        embedding_matrix[i] = embedding_vector[:embedding_dimension]
+
+print(embedding_matrix.shape)
+
+embedding_layer = Embedding(embedding_matrix.shape[0], embedding_matrix.shape[1],
+                            weights=[embedding_matrix], input_length=1000)
+
+
+model = Sequential()
+
+# utilizzo il layer embedded di glove
+model.add(embedding_layer)
+
+model.add(Dropout(0.2))
+model.add(Conv1D(64, 5, activation='relu'))
+model.add(MaxPooling1D(pool_size=4))
+model.add(LSTM(128))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(data, np.array(balanced_labels), batch_size=64, validation_split=0.25, epochs=3)
+
+# save the tokenizer and model
+#with open("keras_tokenizer.pickle", "wb") as f:
+#    pickle.dump(tokenizer, f)
+#model.save("FakeNewsDetection_model_giusto.hdf5")
