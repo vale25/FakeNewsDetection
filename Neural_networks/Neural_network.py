4d686faac50160c1e6a54083719487299b7aa003
vale25 <valentina.accadia@gmail.com>
2017-11-20 16:43:35 +0100

    clean nn

--- Neural_networks/Neural_network.py
+++ Neural_networks/Neural_network.py
@@ -10,7 +10,7 @@ import numpy as np
 import pickle
 
 t1 = datetime.now()
-with open("trainset_senza_duplicati.json") as f:
+with open("/home/valentina/Documenti/tesi/trainset_senza_duplicati.json") as f:
     articles = f.read().strip().split("\n")
 
 articles = [json.loads(article) for article in articles]
@@ -21,7 +21,8 @@ texts = [article['text'] for article in articles]
 binfake = [0 if article['label'] == "FAKE" else 1 for article in articles]
 balanced_texts = []
 balanced_labels = []
-limit = 100000  # Change this to grow/shrink the dataset
+# usa questo parametro se il dataset e' molto grande per decidere quante news fake e real considerare
+limit = 10000
 neg_pos_counts = [0, 0]
 for i in range(len(texts)):
     polarity = binfake[i]
@@ -34,55 +35,31 @@ print(balanced_labels)
 print(Counter(balanced_labels))
 
 # comincio ad addestrare
-tokenizer = Tokenizer(num_words=20000)
+
+# num_words: le prime n parole piu' frequenti vengono incluse nel vocabolario
+tokenizer = Tokenizer(num_words=30000)
+
 tokenizer.fit_on_texts(balanced_texts)
 sequences = tokenizer.texts_to_sequences(balanced_texts)
-data = pad_sequences(sequences, maxlen=700)
-
-# uso pretrained embeddings con i dati di glove (commenta fino a riga 69 per usare l'embedding di keras
-
-embeddings_index = {}
-glove_data = '/media/valentina/Data/pretrained_data/glove.6B.50d.txt'
-f = open(glove_data)
-for line in f:
-    values = line.split()
-    word = values[0]
-    value = np.asarray(values[1:], dtype='float32')
-    embeddings_index[word] = value
-f.close()
-
-print('Loaded %s word vectors.' % len(embeddings_index))
-
-embedding_dimension = 50
-word_index = tokenizer.word_index
-
-embedding_matrix = np.zeros((len(word_index)+1, embedding_dimension))
-for word, i in word_index.items():
-    embedding_vector = embeddings_index(word)
-    if embedding_vector is not None:
-        # words not found in embedding index will be all-zeros.
-        embedding_matrix[i] = embedding_vector[:embedding_dimension]
-
-print(embedding_matrix.shape)
-
-embedding_layer = Embedding(embedding_matrix.shape[0], embedding_matrix.shape[1],
-                            weights=[embedding_matrix], input_length=700)
-
+print (len(tokenizer.word_index))
+data = pad_sequences(sequences, maxlen=1000)
 
 model = Sequential()
-# decommentare per utilizzare l'embedding di keras
-#model.add(Embedding(20000, 128, input_length=700))
 
-# utilizzo il layer embedded di glove (commenta per usare l'embedding di keras)
-model.add(embedding_layer)
-
+model.add(Embedding(30000, 128, input_length=1000))
 model.add(Dropout(0.2))
 model.add(Conv1D(64, 5, activation='relu'))
 model.add(MaxPooling1D(pool_size=4))
 model.add(LSTM(128))
 model.add(Dense(1, activation='sigmoid'))
+
+# con queste due righe realizzo una rete neurale ricorrente semplice, piu' lenta
+#model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
+#model.add(Dense(1, activation='sigmoid'))
+#
+
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(data, np.array(balanced_labels), batch_size=64, validation_split=0.25, epochs=3)
+model.fit(data, np.array(balanced_labels), batch_size=64, validation_split=0.25, epochs=2)
 
 # save the tokenizer and model
 #with open("keras_tokenizer.pickle", "wb") as f:
