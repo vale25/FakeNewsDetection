1106b5a2ba7fca4f6355a579cd9841593134fe71
luca04 <lucamancini02@gmail.com>
2017-12-01 17:01:00 +0100

    Mongodb

--- TagMe/TagMe_analisys.py
+++ TagMe/TagMe_analisys.py
@@ -1,19 +1,22 @@
 import urllib, json
 #url = "https://tagme.d4science.org/tagme/tag?lang=en&tweet=true&gcube-token=edc123ea-fe22-48c0-90d1-3f8b11c82116-843339462&text= Recent poll show President Obama opening up a small lead over GOP rival Mitt Romney"
-#url = "https://tagme.d4science.org/tagme/tag?lang=en&include_abstract=true&include_categories=true&gcube-token=edc123ea-fe22-48c0-90d1-3f8b11c82116-843339462&text=Schumacher won the race in Indianapolis"
-url = "https://tagme.d4science.org/tagme/spot?lang=en&gcube-token=edc123ea-fe22-48c0-90d1-3f8b11c82116-843339462&tweet=true&text=Recent poll show President Obama opening up a small lead over GOP rival Mitt Romney"
+#url = "https://tagme.d4science.org/tagme/tag?lang=en&include_abstract=true&include_categories=true&gcube-token=edc123ea-fe22-48c0-90d1-3f8b11c82116-843339462&text=Killing Obama administration rules, dismantling Obamacare and pushing through tax reform are on the early to-do list."
+url = "https://tagme.d4science.org/tagme/spot?lang=en&gcube-token=edc123ea-fe22-48c0-90d1-3f8b11c82116-843339462&tweet=true&text=Killing Obama administration rules, dismantling Obamacare and pushing through tax reform are on the early to-do list."
 response = urllib.urlopen(url)
 data = json.loads(response.read())
+print data
 a = data["spots"]
+list1 = []
 for elem in a:
     if(elem["lp"] >= 0.1):
         print elem["spot"]
+        list1.append(str(elem["spot"]))
 
 #import nltk
 #nltk.download('wordnet')
 from nltk.corpus import wordnet
-
-list1 = ['Ass','chair', 'Clinton']
+print(list1)
+#list1 = ['Ass','chair', 'Clinton']
 #list2 = ['Fair', 'govern', 'happy']
 list = []
 cont = -1
@@ -28,6 +31,13 @@ for i in range(len(list1)):
 
 
 print(list)
+if list != []:
+    tot = 0
+    for i in range(len(list)) :
+        tot = tot + list[i]
+
+    media = tot/len(list)
+    print(media)
 
 
 
