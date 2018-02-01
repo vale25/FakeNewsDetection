8c19950be2731ca32507f307bf31e8416e6b23d8
vale25 <valentina.accadia@gmail.com>
2018-02-01 15:20:12 +0100

    topic detection per news real e fake con tagme

---
+++ Tagme_similarity_analysis/topicDetection.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+# questa classe individua attraverso Tagme gli argomenti più rilevanti per i due pool di notizie
+# real e fake per farne un confronto
+# Fare lo stesso anche per i soli titoli
+
+
+import os, sys
+import tagme, ast
+from unidecode import unidecode
+import numpy, time
+from itertools import islice
+from Variables import tagme_token, real_news_json, fake_news_json
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+tagme.GCUBE_TOKEN = tagme_token
+
+
+topics_real, topics_fake = {}, {}
+values_real, values_fake = {}, {}
+
+
+def retrieveTopics(text, topics):
+    annotations = tagme.annotate(text)
+    for ann in annotations.get_annotations(0.3):
+        topics.setdefault(ann.entity_title, []).append(ann.score)
+
+
+def computeValue(topics, values):
+    keys = topics.keys()
+    for k in keys:
+        scores = topics[k]
+        mean = numpy.mean(scores)*len(scores)
+        values[k] = mean
+
+i=1
+with open(real_news_json, "r") as real:
+
+    #n = 20
+    #head = list(islice(real, n))
+    #for line in head:
+
+    for line in real:
+        article = ast.literal_eval(line)
+        text = article['text']
+        retrieveTopics(text, topics_real)
+        print i
+        i+=1
+real.close
+
+computeValue(topics_real, values_real)
+topics_real_sorted = sorted(values_real.items(), key=lambda x: x[1], reverse=True)
+top100_real = topics_real_sorted[:100]
+
+print "scrivo su file..."
+
+with open("top100_real", "w") as top:
+
+    for elem in top100_real:
+        top.write(str(elem[0])+" "+str(elem[1])+"\n")
+top.close
+
+j=1
+with open(fake_news_json, "r") as fake:
+    for line in fake:
+        article = ast.literal_eval(line)
+        text = article['text']
+        retrieveTopics(text, topics_fake)
+    print j
+    j+=1
+fake.close
+
+computeValue(topics_fake, values_fake)
+topics_fake_sorted = sorted(values_fake.items(), key=lambda x: x[1], reverse=True)
+top100_fake = topics_fake_sorted[:100]
+
+print "scrivo su file..."
+
+with open("top100_fake", "w") as top2:
+
+    for elem in top100_fake:
+        top.write(str(elem[0])+" "+str(elem[1])+"\n")
+top2.close
+
+
