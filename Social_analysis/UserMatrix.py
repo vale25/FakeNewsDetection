9506a4c7cc7f43937698f6e211def3d15cf8bc0f
luca04 <lucamancini02@gmail.com>
2018-02-13 13:35:07 +0100

    User Matrix features

--- Social_analysis/UserMatrix.py
+++ Social_analysis/UserMatrix.py
@@ -27,8 +27,79 @@ for doc in news:
 record = collection.find({"_id": {"$in":id_news} })
 print record.count()
 
+i = 0
+with open('Prova.txt', 'w') as the_file:
+    for doc in record:
 
-#Query per prendere gli id
+        if i < 2:
+            tweet = doc['Tweet']
+            user = tweet["user"]
+            user_id = user['id']
+            print("userId")
+            print(user_id)
+
+            screen_name = user['screenName']
+            Screen_name = len(screen_name)
+            Numbers = (re.findall(r"\d+", screen_name))
+            if Numbers == []:
+                NumbersElem = 0
+            else:
+                Numbers2 = list(Numbers[0])
+                NumbersElem = len(Numbers2)
+
+
+            print ("Screen name lenght:")
+            print Screen_name
+            print ("numbers in screen Name")
+            print NumbersElem
+
+            Username = user['name']
+            User_Name_lenght = len(Username)
+
+            print("UserName lenght:")
+            print User_Name_lenght
+
+            UserBio = user['description']
+            User_bio_lenght = len(UserBio)
+
+            print("User biography lenght:")
+            print User_bio_lenght
+
+            UserFriends = user['friendsCount']
+
+            print("User friendsCount:")
+            print UserFriends
+
+            UserFollowers = user['followersCount']
+
+            print("User followersCount:")
+            print UserFollowers
+
+            UserFavourites = user['favouritesCount']
+
+            print("User favouritesCount:")
+            print UserFavourites
+
+            Userstatuses = user['statusesCount']
+
+            print("User statusesCount:")
+            print Userstatuses
+
+            Userlisted = user['listedCount']
+
+            print("User listedCount:")
+            print Userlisted
+
+            '''MediaPostScritti = Userstatuses / age
+            print("Media post:")
+            print(MediaPostScritti)'''
+
+            the_file.write(str(user_id) + "," + str(Screen_name) + "," + str(NumbersElem) + "," + str(User_Name_lenght) + "," + str(User_bio_lenght) + "," + str(UserFriends) + "," + str(UserFollowers) + "," + str(UserFavourites) + "," + str(Userstatuses) + "," + str(Userlisted) + "\n" )
+
+
+        i+=1
+
+''''#Query per prendere gli id
 user_id = collection.find( {},  {"Tweet.user.id":1, "_id":0}).limit(100)
 
 #Query per prendere gli screenName
@@ -225,5 +296,5 @@ for doc in UserMediaCount:
     media = tweet['mediaEntities']
     User_mediaCount.append(len(media))
 
-print("User mediaCount:")
-print User_mediaCount
+print("User mediaCount:")'''
+#print User_mediaCount
